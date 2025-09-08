6001bbf83387013dc4c7091e0f35ec3bc303dc93
Kedap <kedap.dev@protonmail.com>
2025-09-07 22:56:28 -0600

    feat: agregando OpenCSV

--- src/main/java/com/codedestroyers/Hito.java
+++ src/main/java/com/codedestroyers/Hito.java
@@ -1,4 +1,64 @@
 package com.codedestroyers;
 
-public record Hito(String anio, String titulo, String descripcion, String imagen_alt) {
+import com.opencsv.bean.CsvBindByPosition;
+
+public class Hito {
+
+    @CsvBindByPosition(position = 0)
+    private String anio;
+
+    @CsvBindByPosition(position = 1)
+    private String titulo;
+
+    @CsvBindByPosition(position = 2)
+    private String descripcion;
+
+    @CsvBindByPosition(position = 3)
+    private String imagen_alt;
+
+    // Constructor requerido por CsvToBean
+    public Hito() {
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen_alt() {
+        return imagen_alt;
+    }
+
+    public void setImagen_alt(String imagen_alt) {
+        this.imagen_alt = imagen_alt;
+    }
+
+    @Override
+    public String toString() {
+        return "Hito{" +
+                "anio='" + anio + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", imagen_alt='" + imagen_alt + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/codedestroyers/Main.java
+++ src/main/java/com/codedestroyers/Main.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Amarillo");
         CSVParser convertidor = new CSVParser("src/main/resources/datos.csv");
         List<Hito> datos = convertidor.obtenerDatos();
         System.out.println(datos);
